1bf848fc32e2e7912180d740ccf505da30807527
cairocen <marvin.centeno@unah.hn>
2024-04-09 16:02:47 -0600

    Ajustes a pagina de inicio

---
+++ app/ui/td-logo.tsx
@@ -0,0 +1,13 @@
+import { ChevronDoubleRightIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function TdLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <ChevronDoubleRightIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">NOC Transformación Digital</p>
+    </div>
+  );
+}
